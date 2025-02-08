270e3867ce4ed0e786f3f13f83c2e853cb52c6ff
George Paci <george.paci@excella.com>
2025-02-08 17:09:54 -0500

    Set indent based on first two lines, instead of always two spaces

--- indented.py
+++ indented.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+import sys
 import datetime
 import re
 import os
 import html
+
+DEBUG=True
+
+def debug(*args):
+    if DEBUG:
+        print(*args)
 
 def read_indented_data_from_fn(filename):
     return read_indented_data_from_file(open(filename))
@@ -17,33 +24,62 @@ def read_indented_data_from_lines(lines):
 
     return parse_indented_data(lines)
 
-def indent(s):
-    for i in range(len(s)):
-        if s[i] != ' ':
-            return int(i/2)
-    return int(len(s)/2)
+def get_indent_and_label(s):
+    i = 0
+    while s.startswith(indent_string):
+        i += 1
+        s = s[len(indent_string):]
+    if s.startswith(indent_separator):
+        s = s[len(indent_separator):]
+    return i, s
+
+# default to four spaces:
+indent_string = '    '
+
+def guess_indent_string(lines):
+    # check for org-mode outline format:
+    if lines[0][0] == '*':
+        debug('org-mode')
+        return '*', ' '
+    # must be spaces or tabs, with no separator:
+    i = 0
+    for line in lines:
+        if line.lstrip() != line:
+            break
+    guess = line[:-len(line.lstrip())]
+    if guess:
+        return guess, ''
+    return indent_string, ''
 
 def parse_indented_data(lines):
+    "This parses a forest, and returns a forest (a list of trees)"
+    global indent_string, indent_separator
+    indent_string, indent_separator = guess_indent_string(lines)
+    debug('indent string is "%s"' % indent_string)
     pos = 0
     children = []
     ll = len(lines)
+    # now we split each line into a (numeric) indent and a label:
+    indented_lines = [get_indent_and_label(line) for line in lines]
+    debug(indented_lines)
+    outer_indent = indented_lines[0][0]
     while pos < ll:
-        child, pos = parse_indented_data_inner(lines, pos, 0)
+        child, pos = parse_indented_data_inner(indented_lines, pos, outer_indent)
         children.append(child)
     return children
 
-"From the top, call with level = 0 repeatedly"
-def parse_indented_data_inner(lines, pos, level):
-    myline = lines[pos]
-    if not indent(myline) == level:
+def parse_indented_data_inner(indented_lines, pos, level):
+    "From the top, call with level = outermost level# repeatedly"
+    ind, label = indented_lines[pos]
+    if not ind == level:
+        debug('expected level %d, got %d, "%s"' % (level, ind, label))
         print('ERROR')
-    label = myline.lstrip()
     children = []
-    ll = len(lines)
+    ll = len(indented_lines)
     pos += 1
-    while pos < ll and indent(lines[pos]) > level:
-        if indent(lines[pos]) == level + 1:
-            child, pos = parse_indented_data_inner(lines, pos, level + 1)
+    while pos < ll and indented_lines[pos][0] > level:
+        if indented_lines[pos][0] == level + 1:
+            child, pos = parse_indented_data_inner(indented_lines, pos, level + 1)
             children.append(child)
     if children:
         return [label, children], pos
@@ -54,5 +90,8 @@ def main(args):
     print(read_indented_data_from_file(sys.stdin))
 
 if __name__ == '__main__':
-    import sys
     main(sys.argv[1:])
+
+# TODO:
+# __ option to print tree as JSON
+# __ option to Python pretty-print tree
